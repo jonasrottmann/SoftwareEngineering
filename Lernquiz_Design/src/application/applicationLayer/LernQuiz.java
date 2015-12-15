258789a2a090b1b942c9c0727ed261a08162d6dc
timbierenbreier <timbierenbreier@gmx.de>
2015-12-15 14:43:53 +0100

    GEIL

---
+++ Lernquiz_Design/src/application/applicationLayer/LernQuiz.java
@@ -0,0 +1,20 @@
+package application.applicationLayer;
+public class LernQuiz {
+
+	public void selbstTesten(boolean selbstTesten) {
+		return;
+	}
+
+	public void wissensstandszeigerErhoehen(Kategorie kategorie) {
+		return;
+	}
+
+	public void antwortPruefen(int antwortNummer) {
+		return;
+	}
+
+	public void kategorieSetzen(Kategorie kategorie) {
+		return;
+	}
+	
+}
---
+++ Lernquiz_Design/src/application/applicationLayer/Wissensstandszeiger.java
@@ -0,0 +1,5 @@
+package application.applicationLayer;
+
+public class Wissensstandszeiger {
+
+}
